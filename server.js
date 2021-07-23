d35658de969528bbd2c7f5b4b44099bcb27c31c8
Cannon-Cloud <clarence@cannon.cloud>
2021-07-23 11:07:28 -0700

    Connection to MogoDB

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 import express from "express";
 import { readdirSync } from "fs";
+import cors from "cors";
+import mongoose from "mongoose";
+const morgan = require("morgan");
+require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
+app.use(morgan("dev"));
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("DB Connection Error: ", err));
+
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
