773fc964092298cacc8af8224840cbb4b8def8f2
Cannon-Cloud <clarence@cannon.cloud>
2021-07-23 13:18:54 -0700

    Added user auth with validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
 import express from "express";
 
-import { showMessage, register } from "../controllers/auth";
+import { register } from "../controllers/auth";
 
 const router = express.Router();
 
-router.get("/:message", showMessage);
 router.post("/register", register);
 
 module.exports = router;
