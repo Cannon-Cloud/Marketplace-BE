555c7949c73f812065976bc9aec1e711b31085c8
Cannon-Cloud <clarence@cannon.cloud>
2021-07-23 10:46:56 -0700

    Added Controllers

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { showMessage } from "../controllers/auth";
+
+const router = express.Router();
+
+router.get("/:message", showMessage);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("==========> Node SERVER");
+import express from "express";
+import { readdirSync } from "fs";
+
+const app = express();
+
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+app.listen(8000, () => console.log(`Server is running on port 8000`));
